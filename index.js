e2e8d21a209e6ea2dedc494f1912bbc20215334b
clevmania <lawrence@grandilo.com>
2018-08-29 21:16:00 +0100

    created route for posting todo

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const user = require('./model/user');
 // create an express application
 var app = express();
 
+// route to post todo
+app.post('/todo',(req,res)=>{
+    var todo = new todo({
+        text : req.body.text
+    });
+
+    todo.save().then((doc)=>{
+        res.send(doc);
+    },(err)=>{
+        res.status(400).send(err);
+    })
+});
+
 // listening for services
 app.listen(3000,()=>{
     console.log('Started listening for services');
