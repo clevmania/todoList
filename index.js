6b8777d44fbf7914b74efbd15d1f69500c8d60b0
clevmania <lawrence@grandilo.com>
2018-08-29 21:01:08 +0100

    created express application and listen for services

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const express = require('express');
 const mongoose = require('./db/mongoose');
 const todo = require('./model/todo');
 const user = require('./model/user');
+
+// create an express application
+var app = express();
+
+// listening for services
+app.listen(3000,()=>{
+    console.log('Started listening for services');
+});
