10735969e1be8be01ec9b475c417632fe1cd9ccd
clevmania <lawrence@grandilo.com>
2018-08-30 03:30:11 +0100

    remove error block

--- pg/mongoose-queries.js
+++ pg/mongoose-queries.js
@@ -4,22 +4,25 @@ const {Todo} = require('./../model/todo');
 var id = '5b87070e3c10cf1151c9872';
 
 //finding todos
-Todo.find({_id : id}).then((todos)=>{
+Todo.find({
+    _id : id
+}).then((todos)=>{
     console.log('Showing all todos', todos);
-},(err)=>{
-    console.log('could not find todo',err);
 });
 
 // find by id
 // specifically for finding by id
-Todo.findById(did).then((todo)=>{
+Todo.findById(id).then((todo)=>{
+    if(!todo){
+        return console.log('id not found');
+    }
     console.log('Showing todo',todo);
-},(err)=>{
-    console.log('could not show todo',err);
 });
 
 // find one
 // search todo with parameters other than id
-Todo.findOne({_id : id}).then((todo)=>{
+Todo.findOne({
+    _id : id
+}).then((todo)=>{
     console.log('Showing todo',todo);
 });
